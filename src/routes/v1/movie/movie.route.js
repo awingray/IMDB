89e8eb001bc8264ba3310577aca60a62009dfecf
AntoninThioux <antonin.thioux@gmail.com>
2020-12-19 00:51:40 +0100

    removed .idea file

--- src/routes/v1/movie/movie.route.js
+++ src/routes/v1/movie/movie.route.js
@@ -1,9 +1,16 @@
 var express = require("express");
 var router = express.Router();
-var movie_controller = require("../../../database/controllers/movie.controller");
-
+//var movie_controller = require("../../../database/controllers/movie.controller");
+/*
 router.get("/", movie_controller.getMovies);
 router.get("/detail", movie_controller.movieDetail);
 router.post("/add", movie_controller.addMovies);
+ */
+
+//TODO: add real functions.
+router.get("/movie/:id", (req, res) => {res.send("Hello World")});
+router.get("/statistics", (req, res) => {res.send("Hello World")});
+router.put("/movie/:id", (req, res) => {res.send("Hello World")});
+router.delete("/movie/:id", (req, res) => {res.send("Hello World")});
 
 module.exports = router;
