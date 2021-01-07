78a41fb6d679b2f63b9d9b31a06fe379cfa98140
AntoninThioux <antonin.thioux@gmail.com>
2021-01-07 14:37:25 +0100

    fixed stats bug

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -41,14 +41,12 @@ function getMean(distribution) {
  * @return {NaN|number} The median.
  */
 function getMedian(distribution){
-    let runningCount = 0;
-    let totalCount = distribution.reduce((a, b) => a + b, 0);
-    if (totalCount === 0) return NaN;
-    for (let i = 0; i < distribution.length; i++){
-        runningCount += distribution[i];
-        if (runningCount === totalCount/2) return i/10 + 0.05;
-        if (runningCount > totalCount/2) return i/10;
-    }
+    let population = distribution.reduce((a, b) => a + b, 0);
+    if (population === 0) return NaN;
+    let i, j, count = 0;
+    for (i = 0; count < population/2; i++) count += distribution[i];
+    for (j = i; count === population/2; j++) count += distribution[j];
+    return (i+j-2)/20;
 }
 
 /**
@@ -69,8 +67,8 @@ function getMode(distribution){
 function getStandardDeviation(distribution){
     let count = distribution.reduce((a, b) => a + b, 0);
     let mean = getMean(distribution);
-    if (count === 0) return NaN;
-    return Math.sqrt(distribution.reduce((a, b) => a + (b/10 - mean) * (b/10 - mean), 0) / (count - 1));
+    if (count < 2) return NaN;
+    return Math.sqrt(distribution.reduce((a, b, i) => a + b * (i/10 - mean) * (i/10 - mean), 0) / (count - 1));
 }
 
 /**
