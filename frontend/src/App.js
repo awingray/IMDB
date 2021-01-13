8a8b22b0c6475a42057180821e1623f03b8225e0
a2504 <59727073+a2504@users.noreply.github.com>
2021-01-13 12:46:04 +0100

    addMovie page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import AllMovies from './pages/AllMovies';
+import AddMovie from './pages/AddMovie'
 
 function App() {
   return (
     <div>
       <Navbar/>
       <AllMovies/>
+      <Router>
+        <Switch>
+          <Route path="/addmovieform"exact component={() => <AddMovie />} />
+        </Switch>
+      </Router>
       </div>
   );
 }
