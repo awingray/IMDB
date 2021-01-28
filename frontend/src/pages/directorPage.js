4a18e71cab876e5ef7a31ea0ca3e5cf5e5113057
AntoninThioux <antonin.thioux@gmail.com>
2021-01-28 20:31:10 +0100

    added directorPages

--- frontend/src/pages/directorPage.js
+++ frontend/src/pages/directorPage.js
@@ -1,11 +1,83 @@
 import React, {Component} from "react";
+import {Link} from "react-router-dom";
+import MovieTable from "../components/movieTable";
 
 class DirectorPage extends Component {
-    render() {
+    state = {
+        loading: true,
+        response: {
+            name: "Ron Howard",
+            gender: "Man",
+            dob: "March 1, 1954",
+            bio: "Academy Award-winning filmmaker Ron Howard is one of this generation's most popular directors." +
+                " From the critically acclaimed dramas A Beautiful Mind (2001) and Apollo 13 (1995) to the hit comedies Parenthood (1989) and Splash (1984)," +
+                " he has created some of Hollywood's most memorable films...",
+            movies: [
+                {
+                    "movie_uri": "http://localhost:4000/api/movies/5fca75c44e2e9e0d181ea696",
+                    "title": "A Beautiful Mind",
+                    "year": 2001,
+                    "users_rating": 8.2,
+                    "votes": "819,184",
+                    "image_url": "https://m.media-amazon.com/images/M/MV5BMzcwYWFkYzktZjAzNC00OGY1LWI4YTgtNzc5MzVjMDVmNjY0XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL__QL50.jpg"
+                },
+                {
+                    "movie_uri": "http://localhost:4000/api/movies/5fca76824e2e9e0d181f747d",
+                    "title": "Angels & Demons",
+                    "year": 2009,
+                    "users_rating": 6.7,
+                    "votes": "265,652",
+                    "image_url": "https://m.media-amazon.com/images/M/MV5BMjEzNzM2MjgxMF5BMl5BanBnXkFtZTcwNTQ1MTM0Mg@@._V1_UX182_CR0,0,182,268_AL__QL50.jpg"
+                },
+                {
+                    "movie_uri": "http://localhost:4000/api/movies/5fca76894e2e9e0d181f7a40",
+                    "title": "Apollo 13",
+                    "year": 1995,
+                    "users_rating": 7.6,
+                    "votes": "259,592",
+                    "image_url": "https://m.media-amazon.com/images/M/MV5BNjEzYjJmNzgtNDkwNy00MTQ4LTlmMWMtNzA4YjE2NjI0ZDg4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL__QL50.jpg"
+                },
+                {
+                    "movie_uri": "http://localhost:4000/api/movies/5fca75c44e2e9e0d181ea6f6",
+                    "title": "Backdraft",
+                    "year": 1991,
+                    "users_rating": 6.7,
+                    "votes": "69,297",
+                    "image_url": "https://m.media-amazon.com/images/M/MV5BZTFjOWVkNjUtYTdkOC00MGQ5LWI5OTQtMTIyYzQwN2UzMDgzXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_UX182_CR0,0,182,268_AL__QL50.jpg"
+                }]
+        },
+        error: false
+    };
+
+    getLinks(){
         return (
-            <React.Fragment>
-                <p>DirectorPage TODO!</p>
-            </React.Fragment>
+            <div className="row">
+                <div className="col-md-6>">
+                    <Link className="text-secondary" to="/search-director">{"< Search other directors"}</Link>
+                </div>
+            </div>
+        );
+    }
+
+    render() {
+        let {name, dob, bio, movies} = this.state.response;
+        return (
+            <div className="container mt-2">
+                <div className="card">
+                    <div className="card-header">
+                        <h3 className="text-center"> Director Details </h3>
+                        {this.getLinks()}
+                    </div>
+                    <div className="card-body">
+                        <ul className="list-group">
+                            <li className="list-group-item"> <b>Name:</b> {name} </li>
+                            <li className="list-group-item"> <b>Date of Birth:</b> {dob} </li>
+                            <li className="list-group-item"> <b>Biography:</b> {bio} </li>
+                            <li className="list-group-item"> <b>Movies:</b> <MovieTable movies={movies.slice(0,10)}/></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
--- frontend/src/pages/directorSearchPage.js
+++ frontend/src/pages/directorSearchPage.js
@@ -1,11 +1,66 @@
 import React, {Component} from "react";
+import Form from 'react-bootstrap/Form';
+import PersonTable from "../components/personTable";
 
 class DirectorSearchPage extends Component {
+    state = {
+        loading: true,
+        response: {
+            directors: [
+                {name: "Director 1", dob: "January 1, 1980"},
+                {name: "Director 2", dob: "January 1, 1981"},
+                {name: "Director 3", dob: "January 1, 1982"},
+                {name: "Director 4", dob: "January 1, 1984"},
+                {name: "Director 5", dob: "January 1, 1988"},
+                {name: "Director 6", dob: "January 1, 1996"},
+                {name: "Director 7", dob: "January 1, 2000"},
+                {name: "Director 8", dob: "December 14, 1980"},
+                {name: "Director 9", dob: "December 12, 1980"},
+                {name: "Director 10", dob: "December 10, 1980"}
+            ]
+        },
+        error: false,
+        page: 0
+    }
+
+    handleChange() {
+        // TODO!
+    }
+
+    handleSearch() {
+        // TODO!
+    }
+
     render() {
         return (
-            <React.Fragment>
-                <p>DirectorSearchPage TODO!</p>
-            </React.Fragment>
+            <div className="container mt-2">
+                <div className="card">
+                    <div className="card-header">
+                        <h3 className="text-center"> Search Director </h3>
+                    </div>
+                    <div className="card-body">
+                        <Form>
+                            <div className="row">
+                                <div className="col-sm">
+                                    <Form.Control type="text" onChange={this.handleChange()} placeholder="name"/>
+                                </div>
+                                <div className="col-2">
+                                    <Form.Control className="bg-primary text-white" type="submit" value="Search"
+                                                  onClick={this.handleSearch()}/>
+                                </div>
+                            </div>
+                        </Form>
+                        <div className="m-4">
+                            <PersonTable people={this.state.response.directors}/>
+                        </div>
+                        <div className="text-center row">
+                            <div className="col-sm text-right"><button type="button" className="btn  btn-secondary disabled"> previous </button></div>
+                            <div className="col-sm align-middle"><p> {this.state.page} </p></div>
+                            <div className="col-sm text-left"><button type="button" className="btn btn-secondary"> next </button></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
