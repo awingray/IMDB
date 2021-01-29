0c7e18cf99c12c648f0ce9c81bccc3bd3d4e1d32
Awin Gray <backend@engineer.com>
2021-01-29 09:02:35 +0700

    movie details from id

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react";
-import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 // Import all website pages.
 import ActorPage from "./pages/actorPage";
@@ -12,6 +12,9 @@ import MovieSearchPage from "./pages/movieSearchPage";
 import MovieStatsPage from "./pages/movieStatsPage";
 import MovieUpdatePage from "./pages/movieUpdatePage";
 
+// Need to change to MoviePage
+import Detail from "./components/detail";
+
 // Import the navigation bar component.
 import NavigationBar from "./components/navigationBar";
 
@@ -20,21 +23,26 @@ class App extends Component {
         return (
             <Router>
                 <div className="sticky-top">
-                    <NavigationBar/>
+                    <NavigationBar />
                 </div>
                 <Switch>
-                    <Route path="/actor-details/:id" exact render={() => <ActorPage/>}/>
-                    <Route path="/search-actor" exact render={() => <ActorSearchPage/>}/>
+                    <Route path="/actor-details/:id" exact render={() => <ActorPage />} />
+                    <Route path="/search-actor" exact render={() => <ActorSearchPage />} />
 
-                    <Route path="/director-details/:id" exact render={() => <DirectorPage/>}/>
-                    <Route path="/search-director" exact render={() => <DirectorSearchPage/>}/>
+                    <Route path="/director-details/:id" exact render={() => <DirectorPage />} />
+                    <Route path="/search-director" exact render={() => <DirectorSearchPage />} />
 
-                    <Route path="/" exact render={() => <Redirect to="/search-movie"/>}/>
-                    <Route path="/create-movie" exact render={() => <MovieAddPage/>}/>
-                    <Route path="/movie-details/:id" exact render={() => <MoviePage/>}/>
-                    <Route path="/search-movie" exact render={() => <MovieSearchPage/>}/>
-                    <Route path="/search-movie/statistics" exact render={() => <MovieStatsPage/>}/>
-                    <Route path="/movie-details/:id/edit" exact render={() => <MovieUpdatePage/>}/>
+                    <Route path="/" exact render={() => <Redirect to="/search-movie" />} />
+                    <Route path="/create-movie" exact render={() => <MovieAddPage />} />
+
+
+
+                    <Route path="/movie-details/:id" exact render={() => <Detail />} />
+
+
+                    <Route path="/search-movie" exact render={() => <MovieSearchPage />} />
+                    <Route path="/search-movie/statistics" exact render={() => <MovieStatsPage />} />
+                    <Route path="/movie-details/:id/edit" exact render={() => <MovieUpdatePage />} />
                 </Switch>
             </Router>
         );
