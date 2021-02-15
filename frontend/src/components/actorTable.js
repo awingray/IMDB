40619bcda238f6e34406002cdd698ada4cae1d3d
AntoninThioux <antonin.thioux@gmail.com>
2021-02-15 23:47:26 +0100

    fixed merge conflicts with main

---
+++ frontend/src/components/actorTable.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Table from "./table";
+import {Link} from "react-router-dom";
+
+class ActorTable extends Table {
+
+}
+
+export default ActorTable;
---
+++ frontend/src/components/directorTable.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Table from "./table";
+import {Link} from "react-router-dom";
+
+const BASE_URL = "http://localhost:4000/api/movies";
+
+class DirectorTable extends Table {
+
+}
+
+export default DirectorTable;
